ecd9eaeb233835b229be7b30e17914eab6499108
beginor <beginor@qq.com>
2025-10-10 07:55:46 +0800

    add config

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    // 将所有带短横线的标签名都视为自定义元素
+                    isCustomElement: (tag) => tag.includes('-')
+                }
+            }
+        })
+    ],
+    esbuild: {
+        legalComments: 'none' // 移除所有注释，包括 license
+    },
+})
