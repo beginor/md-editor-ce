913ce1085027caec3d396a21fae7c03afcdaa1c3
beginor <beginor@qq.com>
2025-10-10 11:47:09 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { defineCustomElement } from 'vue';
 import MdEditorCE from './md-editor.ce.vue';
 import MdPreviewCE from './md-preview.ce.vue';
 
-const MdEditorV3 = defineCustomElement(MdEditorCE);
+customElements.define(
+    'md-editor-v3',
+    defineCustomElement(MdEditorCE)
+);
 
-customElements.define('md-editor-v3', MdEditorV3);
-
-const MdPreviewV3 = defineCustomElement(MdPreviewCE);
-customElements.define('md-preview-v3', MdPreviewV3);
+customElements.define(
+    'md-preview-v3',
+    defineCustomElement(MdPreviewCE)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         vue({
-            template: {
-                compilerOptions: {
-                    // 将所有带短横线的标签名都视为自定义元素
-                    isCustomElement: (tag) => tag.includes('-')
-                }
+            features: {
+                customElement: true
             }
         })
     ],
