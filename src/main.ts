75228414ce9cf4e6762fb35493ab7d4a7e848d30
beginor <beginor@qq.com>
2025-10-10 08:24:03 +0800

    add preview custom component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { defineCustomElement } from 'vue';
 
 import MdEditorCE from './md-editor.ce.vue';
+import MdPreviewCE from './md-preview.ce.vue';
 
 const MdEditorV3 = defineCustomElement(MdEditorCE);
 
 customElements.define('md-editor-v3', MdEditorV3);
+
+const MdPreviewV3 = defineCustomElement(MdPreviewCE);
+customElements.define('md-preview-v3', MdPreviewV3);
---
+++ src/md-preview.ce.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, watch, defineComponent } from 'vue';
+import { MdPreview, config } from 'md-editor-v3';
+import screenfull from 'screenfull';
+import katex from 'katex';
+import Cropper from 'cropperjs';
+import mermaid from 'mermaid';
+import highlight from 'highlight.js';
+
+// >=3.0
+import * as prettier from 'prettier';
+import parserMarkdown from 'prettier/plugins/markdown';
+import * as echarts from 'echarts';
+
+// 定义 props
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  }
+});
+
+// config({
+//     iconfontType: 'class',
+// });
+
+const text = ref(props.text);
+
+watch(() => props.text, (val) => {
+  text.value = val;
+});
+</script>
+
+<template>
+  <MdPreview :modelValue="text"></MdPreview>
+</template>
+
+<style lang="css">
+@import '../node_modules/md-editor-v3/lib/preview.css';
+@import '../node_modules/.pnpm/highlight.js@11.11.1/node_modules/highlight.js/styles/atom-one-dark.css';
+@import '../node_modules/.pnpm/katex@0.16.23/node_modules/katex/dist/katex.min.css';
+@import './iconfont.css';
+</style>
